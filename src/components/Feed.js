4a66a7e0c9afc14f9b42be4576772f865e5fd584
TeodorTodorovT <teodortodorov752@gmail.com>
2023-03-27 18:12:15 +0300

    prepared video side in feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+import {Sidebar, Videos} from "./";
+
 
 const Feed = () => (
   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -15,6 +17,14 @@ const Feed = () => (
       <Typography className="copyright" variant="body2" sx={{mt:'1.5', color:'#fff'}}>
         Copyright 2023 Teodor
       </Typography>
+    </Box>
+
+    <Box p={2} sx={{ overflowY: 'auto', height:'90vh', flex: 2}}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+        NEW <span style={{color: '#F31503'}}>videos</span>
+      </Typography>
+
+      <Videos videos={[]}/>
     </Box>
   </Stack>
 );
