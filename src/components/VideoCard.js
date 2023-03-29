2acb7f87a130f416c50672e7a2a6b99e8a969cb0
TeodorTodorovT <teodortodorov752@gmail.com>
2023-03-29 18:26:11 +0300

    design bug fix
    video cards would break on small devices

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -19,7 +19,7 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", sm: "356px", md: "320px",  },
         boxShadow: "none",
         borderRadius: "0",
       }}
@@ -28,12 +28,12 @@ const VideoCard = ({
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: {xs: "100%", sm:"358px", md: "320px"}, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff" maxWidth='320px'>
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
           </Typography>
         </Link>
